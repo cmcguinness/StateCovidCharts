aab86faf4acaec80e87b3adc505a44e3b9e76789
cmcguinness-vlocity <cmcguinness@vlocity.com>
2020-06-20 18:38:35 -0400

    Added estimated active infections

--- main.py
+++ main.py
@@ -85,6 +85,37 @@ def do_doubles(title, state: StateData):
     plt.savefig('images/' + mystate.getLastDate()+'-doubles-'+mystate.getstate()+'.png', dpi=150)
 
 
+def do_actives(title, state: StateData):
+    day_that_people_get_tested  = 5
+    duration_of_disease = 14
+
+    plt.clf()
+    # plt.yscale('log')
+    fig = plt.figure(figsize=(10,5))
+
+    line_data = state.get_column_sma('delta_positive', 7)
+    dates = state.getSparseDates(4)
+    xs = range(len(line_data))
+    actives = np.zeros(len(line_data))
+    for i in range(len(line_data)):
+        for j in range(max(0,i-(day_that_people_get_tested-1)),i+1):
+            actives[j] += line_data[i]
+        for j in range(i,min(len(line_data),i+(duration_of_disease - day_that_people_get_tested))):
+            actives[j] += line_data[i]
+    for j in range(len(line_data)-day_that_people_get_tested,len(line_data)):
+        actives[j] += line_data[-1]*(day_that_people_get_tested-(len(line_data)-j))
+
+
+    plt.bar(xs, actives, color='#CfCfff')
+    plt.xticks(xs, dates[len(dates)-len(line_data):], rotation=45)
+    plt.title(title)
+    plt.figtext(0.5, 0.02, "Chart prepared by Charles McGuinness @socialseercom using data from covidtracking.com", ha="center", fontsize=10)
+    fig.subplots_adjust(top=0.95, bottom=0.225)
+
+    # plt.tight_layout()
+    plt.savefig('images/' + mystate.getLastDate()+'-actives-'+mystate.getstate()+'.png', dpi=150)
+
+
 if __name__ == '__main__':
     # parser = argparse.ArgumentParser(description="Generate a state's Covid-19 charts")
     # parser.add_argument('state', type=str, help='two letter abbreviation of the state (default FL)', default='FL')
@@ -94,3 +125,4 @@ if __name__ == '__main__':
     mystate = StateData(state_short)
     plot_state(state_short,mystate)
     do_doubles('Number of days to double daily positivies in '+mystate.get_state_name(), mystate)
+    do_actives('Estimated number of active cases in '+mystate.get_state_name(), mystate)
